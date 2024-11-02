c43d65af41e1433f80cedf0ca95c7629cf2f4086
Austin <austin0629@gmail.com>
2024-11-02 10:06:08 +0800

    Initial Commit

---
+++ ebook/ebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebookApp.swift
+//  ebook
+//
+//  Created by Mac on 2024/11/2.
+//
+
+import SwiftUI
+
+@main
+struct ebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
